f7f115d5d74af9ede5468fc42df9d1fd5bcd7aec
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2024-07-04 12:54:49 +0400

    jjjj

--- src/app/features/feature1/feature1.component.ts
+++ src/app/features/feature1/feature1.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MContainerComponent } from '../../m-framework/m-container/m-container.component';
+//Step 1: Import 
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,10 +14,15 @@ import { Router } from '@angular/router';
 })
 export class Feature1Component {
 
+  //Step 2: Inject
   constructor(private router:Router)
   {
   }
   goHome(){
+    //Step 3: Call to navigate
     this.router.navigateByUrl('/home')
+  }
+  goFeature2(){
+    this.router.navigateByUrl('/feature2');
   }
 }
